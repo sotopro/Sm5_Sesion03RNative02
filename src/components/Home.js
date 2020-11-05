257efa8f8e663179a09ce6276f9768aa8897e790
Daniel Soto <dasg.1993@gmail.comm>
2020-11-04 20:14:01 -0500

    [feature] - create home compoentn & setup tailwinds & router

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <>
+            <div className="App">
+                <div className="container mx-auto">
+                    <h3 className="text center text-3xl mt-20 text-base leading-8 text-black font-bold tracking-wide uppercase">CRUD - Context API & Hook React</h3>
+                </div>
+            </div>
+        </>
+    )
+}
